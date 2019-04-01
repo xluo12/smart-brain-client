0752adb029675e88e818f6132392d6a251c843fc
xluo12 <xluoforhire@gmail.com>
2019-04-01 19:38:59 -0400

    add Logo

--- face_recognition_brain/src/App.js
+++ face_recognition_brain/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-        {/* <Logo />
-        <ImageLinkForm />
-        <FaceRecognition /> */}
+        <Logo />
+        {/* <ImageLinkForm />
+        <FaceRecognition />  */}
       </div>
     );
   }
--- face_recognition_brain/src/components/Navigation/Navigation.js
+++ face_recognition_brain/src/components/Navigation/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navigation = () => {
     return (
-        <nav style={{display: "flex", justifyContent: 'flex-end'}}>
+        <nav style={{display: "flex", justifyContent: 'flex-end', marginRight: '50px'}}>
             <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
         </nav>
     );
