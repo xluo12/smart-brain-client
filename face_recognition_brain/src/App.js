0a1b3642b36e2611aa19b75150e73a375dc2e98f
xluo12 <xluoforhire@gmail.com>
2019-04-01 20:27:57 -0400

    add background

--- face_recognition_brain/src/App.js
+++ face_recognition_brain/src/App.js
@@ -3,13 +3,41 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+import Particles from 'react-particles-js';
+
+// const particlesOptions = {
+//   particles: {
+//     number: {
+//       value: 30,
+//       density: {
+//         enable: true,
+//         value_area: 800
+//       }
+//     }
+//   }
+// }
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles' params={particlesOptions}/>
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognition />  */}
       </div>
