6e51baf3cd1892855612b273290b382afdc6da6a
xluo12 <xluoforhire@gmail.com>
2019-04-01 22:56:52 -0400

    add FacRecognition

--- face_recognition_brain/src/App.js
+++ face_recognition_brain/src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation.js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank.js';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
-// const particlesOptions = {
-//   particles: {
-//     number: {
-//       value: 30,
-//       density: {
-//         enable: true,
-//         value_area: 800
-//       }
-//     }
-//   }
-// }
+const app = new Clarifai.App({
+  apiKey: '897f7f1747284da582d377cdd6322eda'
+ });
 
 const particlesOptions = {
   particles: {
@@ -31,6 +25,49 @@ const particlesOptions = {
 }
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState(
+      { 
+        input: event.target.value,
+      }
+    )
+  }
+
+  onButtonSubmit = () => {
+    this.setState(
+      { 
+        imageUrl: this.state.input,
+      }
+    )
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, "https://samples.clarifai.com/metro-north.jpg")
+    .then(response => this.calculateFaceLocation(response))
+    .catch(err => console.log(err))
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage')
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
@@ -38,8 +75,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition />  */}
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl}/> 
       </div>
     );
   }
