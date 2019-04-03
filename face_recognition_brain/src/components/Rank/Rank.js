b072fe7a2441cea57c99f540bc2f68b9665fb1e8
xluo12 <xluoforhire@gmail.com>
2019-04-03 09:49:58 -0400

    fix entries update & name display

--- face_recognition_brain/src/components/Rank/Rank.js
+++ face_recognition_brain/src/components/Rank/Rank.js
@@ -4,10 +4,10 @@ const Rank = ({name, entries}) => {
     return (
         <div>
             <div className='white f3'>
-                {`${name} , your current rank is...`}
-                <div className='white f1 '>
-                    {entries}
-                </div>
+                {`${name} , your current entry count is...`}
+            </div>
+            <div className='white f1 '>
+                {entries}
             </div>
         </div>
     );
